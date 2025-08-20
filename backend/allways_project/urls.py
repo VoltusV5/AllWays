f8ad053c785f39b217f42793005c909e915e45e5
VoltusV5 <maxt.spb@gmail.com>
2025-08-20 15:18:47 +0300

    CI/CD: тест 8

--- backend/allways_project/urls.py
+++ backend/allways_project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse("Hi, test 7")
+    return HttpResponse("Hi, test 8")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),  # корень сайта — функция home
