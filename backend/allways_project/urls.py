9212c1a8f8e70b57c3c3c90493ec278e84284e74
VoltusV5 <maxt.spb@gmail.com>
2025-08-20 14:40:06 +0300

    CI/CD: обновил deploy.yml тест 3

--- backend/allways_project/urls.py
+++ backend/allways_project/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse("Hi, test 2")
+    return HttpResponse("Hi, test 3")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),  # корень сайта — функция home
