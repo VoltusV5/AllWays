5483903cd95833c47c1c0b0185b57a3251254c5d
VoltusV_SERVER <maxt.spb@gmail.com>
2025-09-17 15:34:26 +0000

    code: сделал admin.py, настроил первоначальную админку

--- backend/allways_project/urls.py
+++ backend/allways_project/urls.py
@@ -1,14 +1,13 @@
 # allways_project/urls.py
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, include, re_path
 from core.views import FrontendAppView  # импорт вашего класса
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # админка
-    # любой другой API- или backend-URL можно здесь добавить, например:
-    # path('api/users/', include('users.urls')),
+    # ... любые другие backend-роуты
 
-    # все остальные пути перенаправляем на Vue
-    re_path(r'^.*$', FrontendAppView.as_view(), name='home'),
+    # Всё остальное отдаём Vue
+    re_path(r'^(?!admin/).*$', FrontendAppView.as_view(), name='home'),
 ]
--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,17 +1,26 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User, Profile, AccountLevel, Notification, SocialLogin, LoginHistory
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 @admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    model = User
-    list_display = ("email", "is_staff", "is_active", "email_verified")
-    search_fields = ("email",)
-    ordering = ("email",)
-
+class CustomUserAdmin(BaseUserAdmin):
+    ordering = ('email',)
+    list_display = ('email', 'is_staff', 'is_active', 'email_verified')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'created_at')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_superuser'),
+        }),
+    )
 
 admin.site.register(Profile)
 admin.site.register(AccountLevel)
 admin.site.register(Notification)
 admin.site.register(SocialLogin)
 admin.site.register(LoginHistory)
+
